e1b81577c83ff735c89ac91172a264241b106276
Çağatay YILDIZ <cagatay.yildiz@baogrenci.com>
2019-11-03 16:34:28 +0300

    uid, post

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var app = require('express')();
 var server = require('http').Server(app);
 var conf = require('./config');
 const bodyParser = require('body-parser');
-var validator = require('validator');
-
-console.log(validator.isEmail('fooasdasd')); 
-
-
+const uuidv1 = require('uuid/v1');
+const { check, validationResult } = require('express-validator');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+console.log(uuidv1())
 
 const Pool = require('pg').Pool
 const db = new Pool({
@@ -26,10 +24,35 @@ app.get('/', function (req, res) {
   res.json({ status: 'success', message: 'Book added.' })
 });
 
-app.post('/',function(req,res){
-  res.status(200).send("son olarak");
+app.post('/category/add', [
+  check('name').not().isEmpty(),
+  check('description').isLength({ min: 3 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  else{
+    var id = uuidv1();
+    var name = req.query.name;
+    var description = req.query.description;
+    db.query(`INSERT INTO public.category(
+      id, name, description, isdelete, addate)
+      VALUES ('`+ id +`','` + name +`','` + description + `',false, now());`,function(err){
+    
+        if(!err){
+          res.status(200).send("İşlem Başarılı!");
+        }
+        else{
+          res.status(400).send("İşlem başarısız. Eksik parametre!")
+        }
+      })
+  }
 })
 
   server.listen(3000, function () {
     console.log('listening on *:' + 3000);
 });
+
+
+
